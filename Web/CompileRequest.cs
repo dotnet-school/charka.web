37a3b908e8b6a7f11a0c5962d161bc657ffa14f7
nishant <nishant.singh87@gmail.com>
2020-10-04 17:53:29 +0530

    Add compiler API.

---
+++ Web/CompileRequest.cs
@@ -0,0 +1,8 @@
+
+namespace Web
+{
+  public class CompileRequest
+  {
+    public string[] Snippet { get; set; }
+  }
+}
---
+++ Web/Controllers/CompileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompileController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hello";
+        }
+        
+        [HttpPut]
+        public string CompileCode(CompileRequest request)
+        {
+            return string.Join(", ", request.Snippet ?? Array.Empty<string>());
+        }
+    }
+}
